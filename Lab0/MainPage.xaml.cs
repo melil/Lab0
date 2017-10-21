9068b9449963b4196d832c5d8a8e3de25eb3a1f4
Илья Мельниченко <melil@ya.ru>
2017-10-21 18:15:27 +0300

    Ready

--- Lab0/MainPage.xaml.cs
+++ Lab0/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Lab0.Model;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -33,24 +34,54 @@ namespace Lab0
         }
 
         string city = "Moscow,ru";
-        string link = "http://api.openweathermap.org/data/2.5/weather?q=";
+        string link = "http://api.openweathermap.org/data/2.5/";
         string appid = "&appid=74fbdf01e3477954c50a164249be6f4f";
-
+        double kalvin = 273.5;
+        string urlWeatherUrl = "weather?q=";
+        string urlForecastUrl = "forecast?q=";
 
         public async void getData()
         {
-
-            string url = link + city + appid;
+           
+            string weatherUrl = link + urlWeatherUrl +  city + appid;
+            string forecastUrl = link + urlForecastUrl + city + appid;
             HttpClient client = new HttpClient();
+            
+            string response = await client.GetStringAsync(weatherUrl);
+            string forecastResponse = await client.GetStringAsync(forecastUrl);
 
-            string response = await client.GetStringAsync(url);
 
-            var data = JsonConvert.DeserializeObject<Rootobject>(response);
-            string tempe = data.main.temp.ToString();
+            var weatherData = JsonConvert.DeserializeObject<Rootobject>(response);
+            var forecastData = JsonConvert.DeserializeObject<forecastMainData>(forecastResponse);
+
+            var forecastListFucker = JsonConvert.DeserializeObject<forecastMainData>(forecastResponse);
+
+            
+            object forecastTest = forecastData.list.Count.ToString();
+            string forecastTexts = Convert.ToString(forecastTest);
+
+            double sredTemp = 0;
+            for (int i = 0; i < forecastData.list.Count; i++) //cчитаем среднюю погоду
+            {
+                sredTemp = forecastData.list[i].main.temp + sredTemp;
+            }
+            sredTemp = sredTemp / forecastData.list.Count;
+
+
+
+            sredTmp_lbl.Text = Convert.ToString(Math.Round(sredTemp,1) + "°C");
+
+            string tempe = weatherData.main.temp.ToString();
             double t0c = Convert.ToDouble(tempe);
-            t0c = Math.Round((t0c - 273.5), 1);
-            object weather = data.weather[0].main.ToString();
+            t0c = Math.Round((t0c - kalvin), 1);
 
+
+
+            city_lbl.Text = weatherData.name.ToString();
+            result_lbl.Text = t0c + "°C";
+
+            //image
+            object weather = weatherData.weather[0].main.ToString();
             string weath = Convert.ToString(weather);
 
             switch (weath)
@@ -81,10 +112,10 @@ namespace Lab0
                     break;
                 default:
                     break;
-                    
+
             }
-            city_lbl.Text = data.name.ToString();
-            result_lbl.Text = t0c + "°C";
+
+
         }
 
         private void btn_voronezh_Click(object sender, RoutedEventArgs e)
---
+++ Lab0/Model/JSONWrapper.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab0.Model
+{
+
+
+    public class Rootobject
+    {
+
+
+        public Coord coord { get; set; }
+
+        public List<Weather> weather { get; set; }
+        public string _base { get; set; }
+        public Main main { get; set; }
+        public int visibility { get; set; }
+        public Wind wind { get; set; }
+        public Clouds clouds { get; set; }
+        public int dt { get; set; }
+        public Sys sys { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public int cod { get; set; }
+    }
+
+    public class Coord
+    {
+        public float lon { get; set; }
+        public float lat { get; set; }
+    }
+
+    public class Main
+    {
+        public float temp { get; set; }
+        public int pressure { get; set; }
+        public int humidity { get; set; }
+        public float temp_min { get; set; }
+        public float temp_max { get; set; }
+    }
+
+    public class Wind
+    {
+        public double speed { get; set; }
+        public double deg { get; set; }
+    }
+
+    public class Clouds
+    {
+        public int all { get; set; }
+    }
+
+    public class Sys
+    {
+        public int type { get; set; }
+        public int id { get; set; }
+        public float message { get; set; }
+        public string country { get; set; }
+        public int sunrise { get; set; }
+        public int sunset { get; set; }
+    }
+
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+
+
+    /*  / forecast 
+        /
+        /    
+        /
+        /
+        /
+        /
+    */
+    public class forecastMainData
+    {
+        public string cod { get; set; }
+        public float message { get; set; }
+        public int cnt { get; set; }
+        public List<forecastList> list { get; set; }
+        public forecastCity city { get; set; }
+    }
+
+    public class forecastCity
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public forecastCoord coord { get; set; }
+        public string country { get; set; }
+    }
+
+    public class forecastCoord
+    {
+        public float lat { get; set; }
+        public float lon { get; set; }
+    }
+
+    public class forecastList
+    {
+        public int dt { get; set; }
+        public forecastMainMain main { get; set; }
+        public forecastWeather[] weather { get; set; }
+        public forecastClouds clouds { get; set; }
+        public forecastWind wind { get; set; }
+        public forecastRain rain { get; set; }
+        public forecastSys sys { get; set; }
+        public string dt_txt { get; set; }
+        public forecastSnow snow { get; set; }
+    }
+
+    public class forecastMainMain
+    {
+        private double _temp;
+        public double temp
+        {
+            get
+            {
+                return _temp;
+            }
+            set
+            {
+                _temp = value - 273.15;
+            }
+        }
+        public float temp_min { get; set; }
+        public float temp_max { get; set; }
+        // public double pressure { get; set; }
+        public float sea_level { get; set; }
+        public float grnd_level { get; set; }
+        public int humidity { get; set; }
+        public float temp_kf { get; set; }
+    }
+
+    public class forecastClouds
+    {
+        public int all { get; set; }
+    }
+
+    public class forecastWind
+    {
+        public float speed { get; set; }
+        public float deg { get; set; }
+    }
+
+    public class forecastRain
+    {
+        public float _3h { get; set; }
+    }
+
+    public class forecastSys
+    {
+        public string pod { get; set; }
+    }
+
+    public class forecastSnow
+    {
+        public float _3h { get; set; }
+    }
+
+    public class forecastWeather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+
+
+}
